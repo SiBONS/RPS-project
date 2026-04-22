92e6ce75403c88943d89d49e464a12455e2cce43
Mile Kostic <kostic.mile@mail.com>
2026-04-22 21:10:29 +0200

    Modified the functionality to disable the buttons when any player reaches 5 rounds

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// moram da deklarisem tri varijable za kamen papir makaze
 let rockChoice = document.getElementById("rock");
 let paperChoice = document.getElementById("paper");
 let scissorsChoice = document.getElementById("scissors");
-//moram da deklarisem varijable za score za human i cpu
 
-// mora da se odvaja sam element od score-a. Ako se spajaju vrednosti i elementi, nece da radi.
 let humanScoreElement = document.getElementById("user-score");
 let cpuScoreElement = document.getElementById("cpu-score");
 
@@ -16,7 +13,6 @@ cpuScoreElement.innerHTML = cpuScore;
 
 let result = document.getElementById("result");
 
-// this function sets out the display of the result and returns an empty string with a timeout
 function revertDefault() {
     setTimeout(() => {
         result.innerHTML = "";
@@ -24,69 +20,92 @@ function revertDefault() {
         paperChoice.style.backgroundColor = "white";
         scissorsChoice.style.backgroundColor = "white";
     }, 1500);
-    }
-
-// paperChoice.addEventListener("click", function() {
-//     result.innerHTML = "Paper!";
-//     revertDefault();
-// })
+}
 
 function cpuPlayer() {
     let cpuChoice = Math.floor(Math.random() * 3 + 1);
-    if(cpuChoice === 1) {
-    rockChoice.style.backgroundColor = "rgba(11, 63, 131, 0.87)";
-    return "rock";
-}   else if(cpuChoice === 2) {
-    paperChoice.style.backgroundColor = "rgba(11, 63, 131, 0.87)";
-    return "paper";
-} else {
-    scissorsChoice.style.backgroundColor = "rgba(11, 63, 131, 0.87)";
-   return "scissors";
+    if (cpuChoice === 1) {
+        rockChoice.style.backgroundColor = "rgba(11, 63, 131, 0.87)";
+        return "rock";
+    } else if (cpuChoice === 2) {
+        paperChoice.style.backgroundColor = "rgba(11, 63, 131, 0.87)";
+        return "paper";
+    } else {
+        scissorsChoice.style.backgroundColor = "rgba(11, 63, 131, 0.87)";
+        return "scissors";
+    }
 }
+
+function updateScore(winner) {
+    if (winner === "human") {
+        humanScore++;
+        humanScoreElement.innerHTML = humanScore;
+    } else if (winner === "cpu") {
+        cpuScore++;
+        cpuScoreElement.innerHTML = cpuScore;
+    }
+    endGame();
 }
 
 rockChoice.addEventListener("click", function() {
     let cpu = cpuPlayer();
-    if(cpu === "rock") {
+    if (cpu === "rock") {
         result.innerHTML = "It is a tie! No points";
-    }else if(cpu === "scissors") {
+    } else if (cpu === "scissors") {
         result.innerHTML = "You won! Rock beats scissors!";
-        humanScoreElement.innerHTML++;
+        updateScore("human"); 
     } else {
         result.innerHTML = "You lose! Paper beats rock!";
-        cpuScoreElement.innerHTML++;
+        updateScore("cpu"); 
     }
     revertDefault();
-})
+});
 
 paperChoice.addEventListener("click", function() {
     let cpu = cpuPlayer();
-    if(cpu === "rock") {
+    if (cpu === "rock") {
         result.innerHTML = "You won! Paper beats rock!";
-        humanScoreElement.innerHTML++;
-    }else if(cpu === "scissors") {
+        updateScore("human");
+    } else if (cpu === "scissors") {
         result.innerHTML = "You lose! Scissors cut out the hell of ya!";
-        cpuScoreElement.innerHTML++;
+        updateScore("cpu");
     } else {
         result.innerHTML = "No points. Paper VS paper.";
     }
     revertDefault();
-})
+});
 
 scissorsChoice.addEventListener("click", function() {
     let cpu = cpuPlayer();
-    if(cpu === "rock") {
+    if (cpu === "rock") {
         result.innerHTML = "You lose! Rock destroyed you!";
-        cpuScoreElement.innerHTML++;
-    }else if(cpu === "scissors") {
+        updateScore("cpu");
+    } else if (cpu === "scissors") {
         result.innerHTML = "It is a tie! No points";
     } else {
-        result.innerHTML = "You won. Cuting the paper weeee!";
-        humanScoreElement.innerHTML++;
+        result.innerHTML = "You won. Cutting the paper weeee!";
+        updateScore("human");
     }
     revertDefault();
-})
+});
 
+function disableButtons() {
+        paperChoice.style.pointerEvents = "none";
+        rockChoice.style.pointerEvents = "none";
+        scissorsChoice.style.pointerEvents = "none";
+        paperChoice.style.opacity = "0.5";
+        scissorsChoice.style.opacity = "0.5";
+        rockChoice.style.opacity = "0.5"
+}
 
-
-
+function endGame() {
+    if (humanScore === 5) {
+        result.innerHTML = "You won this game!";
+        disableButtons();
+        revertDefault();
+    } else if (cpuScore === 5) {
+        result.innerHTML = "Computer wins this game!";
+        disableButtons();
+        revertDefault();
+    }
+}
